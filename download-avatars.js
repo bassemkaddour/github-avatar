e29b7620569303d95651c944797c6d5ffed70332
Bassem Kaddour <bassemkaddour@gmail.com>
2018-10-02 00:55:16 +0000

    added avatar url logging and secret token

--- download-avatars.js
+++ download-avatars.js
@@ -1,12 +1,28 @@
 var request = require('request');
+var token = require('./secrets')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'request',
+      Authorization: 'token ' + token.GITHUB_TOKEN
+    }
+  };
 
+
+  request(options, function(err, response, body) {
+    var result = JSON.parse(body)
+    console.log(result);
+    cb(err, result);
+  });
 }
 
 getRepoContributors('jquery', 'jquery', function (err, result) {
   console.log('Errors:', err);
-  console.log('Result:', result);
+  console.log('Result:', result.forEach( function (elem) {
+    console.log(elem.avatar_url);
+  }));
 });
