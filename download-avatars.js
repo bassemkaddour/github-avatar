da5d0299e88e26f85a2043a0fb856f8aa48f121b
Bassem Kaddour <bassemkaddour@gmail.com>
2018-10-02 02:00:44 +0000

    refactor and added some comments

--- download-avatars.js
+++ download-avatars.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var token = require('./secrets')
+var token = require('./secrets');
 var fs = require('fs');
 
 var repoOwnerInput = process.argv[2];
@@ -7,6 +7,7 @@ var repoNameInput = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+//checks for valid command line input, header with authorization token,
 function getRepoContributors(repoOwner, repoName, cb) {
   if(!repoOwner || !repoName) {
     console.error('Please specify a reop onwer and a repo name!');
@@ -23,32 +24,34 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 
   request(options, function(err, response, body) {
-    var result = JSON.parse(body)
+    var result = JSON.parse(body);
     cb(err, result);
   });
 }
 
+//creates path, and calls download function on appropriate image url
+function makePath(err, result) {
+  if (err) {
+    console.log('Error occurred:', err);
+  }
 
-
-getRepoContributors(repoOwnerInput, repoNameInput, function (err, result) {
-  console.log('Errors:', err);
-  console.log('Result:', result.forEach( function (elem) {
+  result.forEach( function (elem) {
     var path = './avatars/' + elem.login + '.jpg';
     downloadImageByURL(elem.avatar_url, path);
-  }));
-});
+  });
 
+  console.log('Your images have been downloaded to the avatars directory!');
+}
+
+//writing image to folder
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .on('error', function (err) {
       throw err;
     })
 
-    .on('response', function (response) {
-      console.log('Responded', response.statusCode)
-    })
-
     .pipe(fs.createWriteStream(filePath));
-
 }
 
+
+getRepoContributors(repoOwnerInput, repoNameInput, makePath);
